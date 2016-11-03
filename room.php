e490c28b3e68e82ce6a9cc1aae7abfc8c8f67fcd
Alexander <alexander.zybin@gmail.com>
2016-11-03 08:50:34 +0300

    201611030850

--- room.php
+++ room.php
@@ -5,3 +5,36 @@
 require_once($base_url."../func.php");
 $l = db_connect();
 
+function arr2json($a) {
+	function prepareUTF($matches){
+		return json_decode('"'.$matches[1].'"');
+	}
+	
+	return stripslashes(preg_replace_callback('/((\\\u[01-9a-fA-F]{4})+)/', 'prepareUTF',
+		json_encode($a)
+	));
+}
+
+$room_id = intval($_GET['room_id']);
+
+if($room_id) {
+	
+	$room = sqlval("SELECT * FROM `m_ap` WHERE id = '$room_id'");
+	
+	if($room['id']) {
+		
+		echo arr2json(array(
+			'id' => $room['id'],
+			'bigimg' => $room['bigimg'],
+			'fullimg' => $room['fullimg'],
+			'roomspace' => $room['roomspace'],
+			'roomcount' => $room['roomcount'],
+		));
+		
+	} else {
+		echo arr2json(array());
+	}
+	
+} else {
+	echo arr2json(array());
+}
