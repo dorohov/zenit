3786f51fb68ba367cc1e1f602822dd087bf6512b
Alexander <alexander.zybin@gmail.com>
2016-11-29 16:19:55 +0300

    201611291619

--- layouts-room.tpl.php
+++ layouts-room.tpl.php
@@ -36,6 +36,23 @@ $price = sqlval("SELECT SQL_CALC_FOUND_ROWS * FROM price where status='3' and id
 <link href="./css/site.css" rel="stylesheet">
 <link href="./css/animate.min.css" rel="stylesheet">
 <script src="./js/spin.min.js" ></script> 
+
+<!-- Facebook Pixel Code -->
+<script>
+!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+document,'script','https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '694678950663471');
+fbq('track', 'PageView');
+</script>
+<noscript><img height="1" width="1" style="display:none"
+src="https://www.facebook.com/tr?id=694678950663471&ev=PageView&noscript=1"
+/></noscript>
+<!-- DO NOT MODIFY -->
+<!-- End Facebook Pixel Code --> 
+
 	</head>
 	<body>	
  		
@@ -107,7 +124,7 @@ $price = sqlval("SELECT SQL_CALC_FOUND_ROWS * FROM price where status='3' and id
 					</div>
 					<div class="row _lrpc__row2"> 
 						<div class="cols _lrpc__floor">
-							<div class="_lrpc__floor-inner">
+							<div class="_lrpc__floor-inner"  data-mcs-theme="yellow" data-mcs-axis="x">
 								
 								<div id="apart" name="apart" class="_lrpc__floor-apart">
 									<div class="_lrpc__floor-box" style="background-image: url('http://zenit-orel.ru/image/floor/<?=$floor['bigimg'];?>');" data-base-url="http://zenit-orel.ru/image/floor/<?=$floor['bigimg'];?>" >
