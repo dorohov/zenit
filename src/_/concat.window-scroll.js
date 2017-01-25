d455488146b9726a57782d9e42c55940b50a8af0
Alexander <alexander.zybin@gmail.com>
2017-01-25 12:02:09 +0300

    201701251202

--- src/_/concat.window-scroll.js
+++ src/_/concat.window-scroll.js
@@ -1,1 +1,2 @@
+
 var topPos=$(document).scrollTop(),snb=$(".navbar-site");snb.hasClass("scroll-sidebar")?topPos<100?snb.removeClass("scroll-sidebar"):snb.hasClass("open-scroll-sidebar")&&snb.removeClass("open-scroll-sidebar"):topPos>100&&snb.addClass("scroll-sidebar");
